5373c1f1b6e4908542f67c3006fae742a44c2604
Manuella Graciano <manulugraci@gmail.com>
2024-10-16 10:22:38 -0300

    tela remédios

---
+++ remédios/js/javascript.js
@@ -0,0 +1,43 @@
+$(document).ready(function () {
+    $('.js-slider').slick({
+        
+        dots: false,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 5,
+        slidesToScroll: 4,
+        responsive: [
+    
+        {
+            breakpoint: 1024,
+            settings: {
+            slidesToShow: 4,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+            slidesToShow: 3,
+            slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+            }
+        }
+        ]
+    });
+});
